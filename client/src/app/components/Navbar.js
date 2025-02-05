58c9e445c4d43daff4f92451458d971864664d75
Kenjy Jap <kenhiebusiness@gmail.com>
2025-02-04 17:05:50 -0800

    installed styled-components

---
+++ client/src/app/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header>
+      <h1>Kenjy Jap</h1>
+    </header>
+  )
+}
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,7 +1,19 @@
+'use client'
+
+import React from "react";
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+
+const AppContainer = styled.div`
+  background-color: #1e1e1e;
+  min-height: 100vh;
+  padding: 20px 0;
+`;
+
 export default function Home() {
   return (
-    <div>
-      <h1>Kenjy's Portfolio</h1>
-    </div>
+    <AppContainer>
+      <Navbar />
+    </AppContainer>
   );
 }
