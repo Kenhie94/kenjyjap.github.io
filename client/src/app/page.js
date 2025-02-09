540ba5a7ea0464fa19ce9dee8bdf7ce6604650e5
Kenjy Jap <kenhiebusiness@gmail.com>
2025-02-08 18:15:45 -0800

    updates

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,6 +4,7 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import About from "./components/about/About"
 
 const AppContainer = styled.div`
   background-color: #1e1e1e; /* Match the background color of the hero section */
@@ -19,6 +20,7 @@ export default function Home() {
       <Navbar />
       <MainContent>
         <Hero />
+        <About />
       </MainContent>
     </AppContainer>
   );
